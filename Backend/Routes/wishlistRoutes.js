eeda9e9cb9a14899c4df4a438c668d1e751651c7
deepakcs2003 <135646985+deepakcs2003@users.noreply.github.com>
2025-04-11 20:10:48 +0530

    correct the route erro

--- Backend/Routes/wishlistRoutes.js
+++ Backend/Routes/wishlistRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const {removeFromWishlist, getAllWishlistProducts, addToWishlist } = require("../Controller/wishlistController");
+const authToken = require("../Middleware/authMiddleware");
 const router = express.Router();
 
-router.post("/AddToWishList", addToWishlist);
-router.delete("/DeleteToWishList", removeFromWishlist);
-router.get("/GetAllWishList",getAllWishlistProducts);
+router.post("/AddToWishList",authToken,addToWishlist);
+router.delete("/DeleteToWishList", authToken,removeFromWishlist);
+router.get("/GetAllWishList",authToken,getAllWishlistProducts);
 module.exports = router;
